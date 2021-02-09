a5856903c9cb134816a75d4370753ca62c451cb7
Connor Stec <stec.connor@gmail.com>
2021-02-09 16:43:42 -0500

    clean up fe router config

--- browser/src/router/index.js
+++ browser/src/router/index.js
@@ -5,30 +5,16 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/inventory',
-    name: 'Inventory',
-    // route level code-splitting
-    // this generates a separate chunk (inventory.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "inventory" */ '../views/Inventory.vue')
-  },
-  {
-    path: '/contact',
-    name: 'Contact Us',
-    component: () => import(/* webpackChunkName: "contactus" */ '../views/ContactUs.vue')
-  },
-  {
-    path: '/pricing',
-    name: 'Pricing',
-    component: () => import(/* webpackChunkName: "pricing" */ '../views/Pricing.vue')
+  ['inventory', 'Inventory'],
+  ['contact', 'Contact Us', 'ContactUs'],
+  ['pricing', 'Pricing'],
+].map(([pathSlug, name, componentOverride]) => {
+  return {
+    path: `/${pathSlug}`,
+    name,
+    component: () => import(`../views/${componentOverride || name}.vue`),
   }
-]
+});
 
 const router = new VueRouter({
   routes
